63e9a0230380f067966fc57191d41db4abf1e4da
Аттор Азизов <attorazizov@MacBook-Air-Attor.local>
2026-09-02 12:32:53 +0300

    Добавление базы данных

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status,Depends
 from pydantic import BaseModel
 from uuid import uuid4
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine,String,Boolean,select
+from sqlalchemy.orm import sessionmaker,Session,mapped_column,Mapped,DeclarativeBase
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+DATABASE_URL = "postgresql+psycopg://postgres:admin@localhost:15432/postgres"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker[Session](bind=engine)
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(String, primary_key=True,default=lambda: str(uuid4()))
+
+
+class TaskORM(Base):
+    __tablename__ = "tasks"
+    title: Mapped[str] = mapped_column(String, index=True)
+    completed: Mapped[bool] = mapped_column(Boolean, default=False)
+
+class CategoryORM(Base):
+    __tablename__ = "categories"
+    name: Mapped[str] = mapped_column(String, index=True)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_methods=["*"]
 )
+
 
 class TaskSchema(BaseModel):
     id: str
@@ -33,77 +59,80 @@ class CategoryCreateSchema(BaseModel):
 class CategoryUpdateSchema(BaseModel):
     name: str | None = None
 
-class BookSchema(BaseModel):
-    book:str
-
-book = ''
-
-tasks: list[TaskSchema] = []
-categories: list[CategorySchema] = []
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def tasks_to_orm(task: TaskSchema) -> TaskORM:
+    return TaskORM(id=task.id,title=task.title,completed=task.completed)
+
+def categories_to_orm(category: CategorySchema) -> CategoryORM:
+    return CategoryORM(id=category.id,name=category.name)
 
 @app.get("/tasks")
-def read_root():
-    return tasks
+def read_root(db: Session = Depends(get_db)):
+    tasks_from_db = db.scalars(select(TaskORM)).all()
+    return [tasks_to_orm(task) for task in tasks_from_db]
 
 
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
-def create_task(payload: TaskCreateSchema) -> TaskSchema:
-    new_task = TaskSchema(id=str(uuid4()),title = payload.title,completed =False)
-    tasks.append(new_task)
-    return new_task
+def create_task(payload: TaskCreateSchema,db: Session = Depends(get_db)) -> TaskSchema:
+    new_task = TaskORM(title = payload.title,completed =False)
+    db.add(new_task)
+    db.commit()
+    return tasks_to_orm(new_task)
 
 @app.patch('/tasks/{task_id}')
-def update_task(task_id: str,payload: TaskUpdateSchema):
-    for task in tasks:
-        if task.id == task_id:
-            if payload.title:
-                task.title = payload.title
-            if payload.completed is not None:
-                task.completed = payload.completed
+def update_task(task_id: str,payload: TaskUpdateSchema,db: Session = Depends(get_db)):
+    task_update = db.get(TaskORM,task_id)
+    if payload.title:
+        task_update.title = payload.title
+    if payload.completed is not None:
+        task_update.completed = payload.completed
+    db.commit()
+    return task_update
 
-            return tasks
 
 @app.delete('/tasks/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_task( task_id: str):
-    for task in tasks:
-        if task.id == task_id:
-            tasks.remove(task)
+def delete_task( task_id: str,db: Session = Depends(get_db)):
+    task_delete = db.get(TaskORM,task_id)
+    db.delete(task_delete)
+    db.commit()
+    return task_delete
 
 @app.get('/categories')
-def read_categories():
-    return categories
+def read_categories(db: Session = Depends(get_db)):
+    categories_from_db = db.scalars(select(CategoryORM)).all()
+    return [categories_to_orm(category) for category in categories_from_db]
 
 @app.post('/categories', status_code=status.HTTP_201_CREATED)
-def create_category(payload: CategoryCreateSchema)->CategorySchema:
-    new_category = CategorySchema(id=str(uuid4()),name = payload.name)
-    categories.append(new_category)
-    return new_category
+def create_category(payload: CategoryCreateSchema,db: Session = Depends(get_db))->CategorySchema:
+    new_category = CategoryORM(name = payload.name)
+    db.add(new_category)
+    db.commit()
+    return categories_to_orm(new_category)
 
 @app.patch('/categories/{category_id}')
-def update_category(category_id: str,payload: CategoryUpdateSchema):
-    for category in categories:
-        if category.id == category_id:
-            if payload.name:
-                category.name = payload.name
-            return categories
+def update_category(category_id: str,payload: CategoryUpdateSchema,db: Session = Depends(get_db)):
+    category_update = db.get(CategoryORM,category_id)
+    if payload.name:
+        category_update.name = payload.name
+    db.commit()
+    return categories
+        
 
 @app.delete('/categories/{category_id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_category(category_id: str):
-    for category in categories:
-        if category.id == category_id:
-            categories.remove(category)
+def delete_category(category_id: str,db: Session = Depends(get_db)):
+    category_delete = db.get(CategoryORM,category_id)
+    db.delete(category_delete)
+    db.commit()
+    return category_delete
 
-# @app.get("/book")
-# def read_root():
-#     return {f'message:Любимая книга: {book}'}
-
-# @app.post('/book')
-# def get_book(payload:BookSchema):
-#     global book
-#     book = payload.book
-#     return book
 
 
 
